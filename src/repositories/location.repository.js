94cc9843cab68c593cb7c0e3799e14c9d6622509
lee-haeseung <lhs020205@naver.com>
2024-11-09 19:47:30 +0900

    refactor: location repository 수정

--- src/repositories/location.repository.js
+++ src/repositories/location.repository.js
@@ -1,49 +1,31 @@
-import { pool } from "../db.config.js";
+import { prisma } from "../db.config.js";
 
 // location name 조회
 export const getLocationByName = async (locationName) => {
-  const conn = await pool.getConnection();
-
-  try {
-    const [location, ] = await conn.query(
-      `SELECT * FROM location WHERE name = ?`,
-      [locationName]
-    )
-
-    if (location.length == 0) {
-      throw new Error("등록되지 않은 장소입니다.")
+  const location = await prisma.location.findFrist(
+    {
+      where: {name: locationName}
     }
+  );
 
-    return location;
-  } catch (err) {
-    throw new Error(
-      `오류가 발생했어요. 요청 파라미터를 확인해주세요. (${err})`
-    );
-  } finally {
-    conn.release();
+  if (!location) {
+    return null;
   }
+  
+  return location;
 };
 
 // location id 조회
 export const getLocationById = async (locationId) => {
-  const conn = await pool.getConnection();
-
-  try {
-    const [location, ] = await conn.query(
-      `SELECT * FROM location WHERE id = ?`,
-      [locationId]
-    )
-
-    if (location.length == 0) {
-      throw new Error("등록되지 않은 장소입니다.")
+  const location = await prisma.location.findFrist(
+    {
+      where: {id: locationId}
     }
-
-    return location;
-  } catch (err) {
-    throw new Error(
-      `오류가 발생했어요. 요청 파라미터를 확인해주세요. (${err})`
-    );
-  } finally {
-    conn.release();
+  );
+  
+  if (!location) {
+    return null;
   }
+
+  return location;
 };
